2ea548d9c002ff30fdfb6196eb590f619c15f678
Nikolay Krivosheev <krivosheev@thehead.ru>
2025-06-05 21:21:32 +0500

    16-05-24HW.js review issues fix

--- lessons/16-05/16-05-24HW.js
+++ lessons/16-05/16-05-24HW.js
@@ -1,11 +1,17 @@
 function getPath(element, document) {
     if (element === document.body) {
+        if (!element.parentNode) {
+            throw new Error('Element has no parent node');
+        }
+        if (!element || !document) {
+            throw new Error('Element and document must be provided');
+        }
         return element.tagName;
     }
     let idx = 0;
-    const sameLevelElements = element.parentNode.childNodes;
+    const sameLevelElements = element.parentNode.children;
     for (let i = 0; i < sameLevelElements.length; i++) {
-        let iterationItem = sameLevelElements[i];
+        const iterationItem = sameLevelElements[i];
         if (iterationItem === element) {
             return getPath(element.parentNode) + ' > ' + element.tagName + ':nth-child(' + (idx + 1) + ')';
         }
@@ -15,4 +21,4 @@ function getPath(element, document) {
     }
 }
 
-module.exports = { getPath };
+export  { getPath };
--- lessons/16-05/16-05.test.js
+++ lessons/16-05/16-05.test.js
@@ -1,7 +1,6 @@
-import { test } from "node:test";
 import * as jsdom from "jsdom";
-const { getPath } = require('./16-05-24HW');
-
+import {getPath} from "./16-05-24HW";
+import * as assert from "node:assert";
 test('getPath function', () => {
     const { JSDOM } = jsdom;
     const dom = new JSDOM(
@@ -15,4 +14,6 @@ test('getPath function', () => {
     const document = dom.window.document;
     let element = document.querySelector('p');
     expect(getPath(element, document)).toBe('body > div > P:nth-child(1)');
+    assert.ok(document, "Document should be defined");
+    assert.ok(element, "Element should be found");
 });
